f5ce194b252b131c74e7bf83d17e662de1c1b781
Daniel Kayque <daanielkayque@gmail.com>
2026-05-21 14:22:47 -0300

    fix: corrige retorno do login

--- src/controllers/authController.ts
+++ src/controllers/authController.ts
@@ -44,9 +44,13 @@ export class AuthController {
     } catch (err) {
       if (err instanceof Error) {
         console.error('Erro interno ' + err.message);
-        return res.status(500).json({ message: 'Ocorreu um erro inesperado.' });
+        return res
+          .status(500)
+          .json({ message: 'Ocorreu um erro inesperado.' + err.message });
       }
-      return res.status(500).json({ message: 'Ocorreu um erro desconhecido.' });
+      return res
+        .status(500)
+        .json({ message: 'Ocorreu um erro desconhecido.' + err });
     }
   }
 }
--- src/routes/paymentRoutes.ts
+++ src/routes/paymentRoutes.ts
@@ -1,10 +1,15 @@
 import { Router } from 'express';
 import paymentController from '../controllers/paymentController.js';
+import { Auth } from '../middlewares/auth.js';
 
 const router = Router();
 
 const paymentControllers = new paymentController();
 
+const { autenticar } = new Auth();
+
+router.use(autenticar);
+//Todas as rotas aqui embaixo estão protegidas
 router.post('/customers', paymentControllers.createClient);
 router.post('/payment', paymentControllers.createPaymentIntent);
 router.post('/payment/confirm', paymentControllers.confirmPaymentIntent);
